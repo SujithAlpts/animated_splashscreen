f133d536b61aaa581c2da782f58bc4ee3f75e78d
Sujith <158046769+SujithAlpts@users.noreply.github.com>
2024-08-13 21:42:47 +0530

    0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,10 @@
 
 It is animated splshscreen with awsome animations
 
+## 0.0.2
 
+fix some bugs related the package
+
+## 0.0.3
+
+upgraded the dependecy to its lattest version
--- test/animated_splashscreen_test.dart
+++ test/animated_splashscreen_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:alp_animated_splashscreen/animated_splashscreen.dart';
+// import 'package:alp_animated_splashscreen/animated_splashscreen.dart';
 
 // void main() {
 //   test('adds one to input values', () {
